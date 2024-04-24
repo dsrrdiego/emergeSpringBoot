62ae9e6fdef91a3a1ac641d255d09478feca5f4a
Diego Sebastian Rodriguez Recalde <dsrrdiego@hotmail.com>
2024-04-24 16:23:43 -0300

    render

--- src/main/java/emerge/api/emergespring/EmergespringApplication.java
+++ src/main/java/emerge/api/emergespring/EmergespringApplication.java
@@ -25,31 +25,37 @@ public class EmergespringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmergespringApplication.class, args);
-		configureGlobalCORS();
+		// configureGlobalCORS();
 	}
 	
 
-	// @Bean
-	// public WebMvcConfigurer corsConfigurer() {
-	// 	return new WebMvcConfigurer() {
-	// 		@Override
-	// 		public void addCorsMappings(CorsRegistry registry) {
-	// 			registry.addMapping("/**").allowedOrigins("*");
-	// 		}
-	// 	};
-	// }
-	
-	public static void configureGlobalCORS() {
-        CorsRegistry registry = new CorsRegistry();
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                // Opcional: Configura otros parámetros de CORS según sea necesario
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true)
                 .maxAge(3600);
-    }
+			}
+		};
+	}
+	
+	// public static void configureGlobalCORS() {
+    //     CorsRegistry registry = new CorsRegistry();
+    //     registry.addMapping("/**")
+    //             .allowedOrigins("*")
+    //             // Opcional: Configura otros parámetros de CORS según sea necesario
+    //             .allowedMethods("GET", "POST", "PUT", "DELETE")
+    //             .allowedHeaders("Content-Type", "Authorization")
+    //             .exposedHeaders("Authorization")
+    //             .allowCredentials(true)
+    //             .maxAge(3600);
+    // }
 
 
 }
